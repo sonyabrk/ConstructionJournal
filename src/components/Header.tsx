eeeae7bf17f86d769973923e0c41c79a98026c98
Katya <klenovakatusha@gmail.com>
2025-09-20 22:09:27 +0300

    create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,16 @@
-function Header() {
+import profileSvg from "../assets/profile.svg"
 
+function Header() {
+    return (
+        <>
+            <div className="header">
+                <a href="/" className="headerProfile">
+                    <img src={profileSvg} alt="Профиль"/>
+                </a>
+                <p>Электронный журнал</p>
+            </div>
+        </>
+    )
 }
 
 export default Header;
