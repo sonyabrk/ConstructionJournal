90fe3705760e9091c21d3b030a1c59cd89d1e3f9
Katya <klenovakatusha@gmail.com>
2025-09-21 17:27:20 +0300

    Create ObjectCard

---
+++ src/components/ObjectCard.tsx
@@ -0,0 +1,24 @@
+interface ObjectCardProps {
+  address: string;
+  coordinates: { lat: number; lng: number };
+  supervision: string;
+  contractor: string;
+  objectStatus: 'Активно' | 'Не активно' | 'Завершено'; // пример union типа
+}
+
+function ObjectCard({ address, coordinates, supervision, contractor, objectStatus} : ObjectCardProps)
+    {
+    return (
+        <>
+            <div className="card">
+                <p className="cardAddress">Адрес: {address} {coordinates.lat} {coordinates.lng}</p>
+                <p className="card">Служба строительного контроля (ответственный):<br/>{supervision}</p>
+                <p className="card">Подрядчик (ответственный):<br/>{contractor}</p>
+                <p className="cardObjectStatus">Состояние объекта: {objectStatus}</p>
+                <button type="submit" className="objectBtn">Обзор</button>
+            </div>
+        </>
+    )
+}
+
+export default ObjectCard;
