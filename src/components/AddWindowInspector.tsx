2a1abc1c5f6686f82a79ee683188ceefeff64f2e
Katya <klenovakatusha@gmail.com>
2025-09-23 16:30:10 +0300

    create AddWindowInspector

---
+++ src/components/AddWindowInspector.tsx
@@ -0,0 +1,10 @@
+function AddWindowInspector() {
+    return (
+        <>
+        <button className="addViolation">Добавить нарушение</button>
+        <button className="addInitialize">Инициировать лаб. отбор</button>
+        </>
+    )
+}
+
+export default AddWindowInspector
