81751a76a2881f264c64ca35451c375ffe0de0ef
Katya <klenovakatusha@gmail.com>
2025-09-23 16:54:12 +0300

    create WindowInitiationInspector

---
+++ src/components/WindowInitiationInspector.tsx
@@ -0,0 +1,14 @@
+function WindowInitiationInspector() {
+    return (
+        <>
+        <div className="header">Инициирование лабораторного отбора</div>
+        <div className="material">
+            <p className="materialTitle">Материал</p>
+            <input className="materialInput" placeholder="Введите материал" />
+            <button className="initiationButton">Инициировать</button>
+        </div>
+        </>
+    )
+}
+
+export default WindowInitiationInspector
