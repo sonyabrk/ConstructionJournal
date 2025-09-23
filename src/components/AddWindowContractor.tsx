77a7b8056a66ea7ceaeeb4bf17fa1525a972b813
Katya <klenovakatusha@gmail.com>
2025-09-23 17:10:35 +0300

    create AddWindowContractor

---
+++ src/components/AddWindowContractor.tsx
@@ -0,0 +1,10 @@
+function AddWindowContractor() {
+    return (
+        <>
+        <button className="addTTN">ТТН</button>
+        <button className="addCompletedWorks">Отметить выполненные работы</button>
+        </>
+    )
+}
+
+export default AddWindowContractor
